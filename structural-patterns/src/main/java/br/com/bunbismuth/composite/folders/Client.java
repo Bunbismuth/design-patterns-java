a0578a9e16a4ffceaf1172e2e7e6aa1d2e33f412
Thiago Barros <bunbismuth@proton.me>
2023-12-03 15:23:08 -0300

    feat: added Client

---
+++ structural-patterns/src/main/java/br/com/bunbismuth/composite/folders/Client.java
@@ -0,0 +1,43 @@
+package br.com.bunbismuth.composite.folders;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.bunbismuth.composite.folders.model.FileSystemItem;
+import br.com.bunbismuth.composite.folders.model.Folder;
+public class Client {
+  public static FileSystemItem createCompositeFromFile(File file) {
+    if (!file.isDirectory()) return
+        new br.com.bunbismuth.composite.folders.model.File(file.getName());
+
+    List<FileSystemItem> childs = new ArrayList<>();
+    File[] files = file.listFiles();
+
+    assert files != null;
+    for (java.io.File javaFile: files) {
+      childs.add(createCompositeFromFile(javaFile));
+    }
+    return new Folder(file.getName(), childs);
+  }
+  public static void main(String[] args) {
+    FileSystemItem file1 = new br.com.bunbismuth.composite.folders.model.File("f1");
+    FileSystemItem file2 = new br.com.bunbismuth.composite.folders.model.File("f1");
+    FileSystemItem file3 = new br.com.bunbismuth.composite.folders.model.File("f1");
+    FileSystemItem file4 = new br.com.bunbismuth.composite.folders.model.File("f1");
+    FileSystemItem sf1 = new Folder("sf1", List.of(file1));
+    FileSystemItem sf2 = new Folder("sf2", Arrays.asList(file2, sf1));
+    FileSystemItem folder = new Folder("folder",
+        Arrays.asList(sf2, file3, file4));
+
+    folder.print("");
+
+    System.out.println("-----------------");
+
+    String dir = System.getProperty("user.dir");
+    FileSystemItem fromDisk = createCompositeFromFile
+        (new File(dir + File.separator + "src"));
+    fromDisk.print("");
+  }
+}
