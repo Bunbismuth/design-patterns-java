25e950739549bd2420aab97011a3fba6fcea7591
Thiago Barros <bunbismuth@proton.me>
2023-12-03 21:39:37 -0300

    feat: Expresso

---
+++ structural-patterns/src/main/java/br/com/bunbismuth/decorator/coffeeShop/model/Expresso.java
@@ -0,0 +1,14 @@
+package br.com.bunbismuth.decorator.coffeeShop.model;
+
+public class Expresso implements Drink {
+
+  @Override
+  public void serve() {
+    System.out.println("- Adding 50ml of expresso");
+  }
+
+  @Override
+  public Double getPrice() {
+    return 1.5;
+  }
+}
